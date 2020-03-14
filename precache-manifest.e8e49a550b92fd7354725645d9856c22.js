bdadbda74f6780e694b9909e8f9d903b09ddbe1e
Елена <s_elena-@bk.ru>
2020-03-14 19:30:23 +0400

    Updates

---
+++ precache-manifest.e8e49a550b92fd7354725645d9856c22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a5ccd8a69238bd37f1707edc4a19472",
+    "url": "/image-catalog/index.html"
+  },
+  {
+    "revision": "92cc1bf43157d0f81fc8",
+    "url": "/image-catalog/static/css/main.5f61f99b.chunk.css"
+  },
+  {
+    "revision": "b1ea26aeb093e8da6f32",
+    "url": "/image-catalog/static/js/2.94f26ffc.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/image-catalog/static/js/2.94f26ffc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92cc1bf43157d0f81fc8",
+    "url": "/image-catalog/static/js/main.5fa6a3a5.chunk.js"
+  },
+  {
+    "revision": "02ae5cdc475af5d22d26",
+    "url": "/image-catalog/static/js/runtime-main.9cfc2430.js"
+  }
+]);
